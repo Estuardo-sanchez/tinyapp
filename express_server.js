377adb034769e674dd3acfdf727e69516b335836
Estuardo <estuardo_101@hotmail.com>
2022-05-08 17:33:10 -0400

    refactored helper functions

--- express_server.js
+++ express_server.js
@@ -22,36 +22,7 @@ app.use(cookieSession({
 
 app.set("view engine", "ejs");
 
-function generateRandomString(length) {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789';
-  let randomString = ' ';
-  let charactersLength = characters.length;
-
-  for (let i = 0; i < 6; i++) {
-    randomString += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return randomString;
-};
-
-const checkForUserEmail = function(email, database) {
-  for (const user in users) {
-    if (database[user].email === email) {
-      return database[user];
-    }
-  }
-  return undefined;
-}
-
-const urlsForUser = (id, database) => {
-  let usersUrl = {};
-
-  for (const shortURL in database) {
-    if (database[shortURL].userID === id) {
-      usersUrl[shortURL] = database[shortURL];
-    }
-  }
-  return usersUrl;
-};
+const { checkForUserEmail, urlsForUser, generateRandomString } = require("./helpers");
 
 const users = {
   "userRandomID": {
@@ -90,7 +61,6 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  console.log(users);
   const userID = req.session.user_id;
   const userUrls = urlsForUser(userID, urlDatabase);
   const templateVars = {
