987acaf2e015625d32fb54f70cfa61fa4f526ecf
Estuardo <estuardo_101@hotmail.com>
2022-05-08 19:44:27 -0400

    eslinted added semi colons and fixed spacing

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -4,13 +4,13 @@ const { checkForUserEmail, generateRandomString, urlsForUser } = require('../hel
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "a"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "a"
   }
 };
@@ -28,35 +28,35 @@ const testUrlDatabase = {
 
 describe('checkForUserEmail', function() {
   it('should return a user with valid email', function() {
-    const user = checkForUserEmail("user@example.com", testUsers)
+    const user = checkForUserEmail("user@example.com", testUsers);
     const expectedUserID = "userRandomID";
     assert.strictEqual(user.id, expectedUserID);
   });
 
-  it("returns undefined with an invalid email", function () {
+  it("returns undefined with an invalid email", function() {
     const user = checkForUserEmail("user5@example.com", testUsers);
     assert.strictEqual(user, undefined);
   });
 });
 
-describe('generateRandomString', () => {
-  it('strings should not be the same', function () {
+describe('generateRandomString',() => {
+  it('strings should not be the same', function() {
     const string0 = generateRandomString();
     const string1 = generateRandomString();
     assert.notEqual(string0, string1);
   });
 });
 
-describe('urlsForUser' , function () {
-  it('should return short urls from url database that match the user id', function () {
+describe('urlsForUser', function() {
+  it('should return short urls from url database that match the user id', function() {
     const user = "user2RandomID";
     const urls = urlsForUser(user, testUrlDatabase);
-    expectedUrls = {
+    const expectedUrls = {
       i3BoGr: {
         longURL: "https://www.google.ca",
         userID: "user2RandomID"
       }
-    }
+    };
     assert.deepEqual(urls, expectedUrls);
-  })
+  });
 });
